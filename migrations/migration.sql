0e02e5c2723e9ace3907e026197e1060bcd1dc07
lffspaniol <luisspaniol12@gmail.com>
2020-07-31 14:13:03 -0300

    data base

--- migrations/migration.sql
+++ migrations/migration.sql
@@ -1,12 +1,29 @@
 -- Migration: create users table
 -- Created at: 2013-10-02 07:03:11
 -- ====  UP  ====
-CREATE TABLE `users`(
-  id int not null primary key auto_increment,
-  name varchar(32) not null,
-  email varchar(255) not null
+CREATE TABLE IF NOT EXISTS hellos(
+  ID  SERIAL PRIMARY KEY,
+  contador  INT DEFAULT 0,
+  messagem  VARCHAR(255) NOT NULL,
+  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  data_alteracao TIMESTAMP, 
+  data_exclusao TIMESTAMP
 );
 
-CREATE UNIQUE INDEX `users_email_uq` ON `users`(`email`);
+create index hellos_id_idx on hellos(ID);
+
+CREATE OR REPLACE FUNCTION update_data_alteracao_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.data_alteracao = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
+CREATE TRIGGER update_hellos_data_alteracao BEFORE UPDATE
+ON hellos FOR EACH ROW EXECUTE PROCEDURE 
+update_data_alteracao_column();
+
 -- ==== DOWN ====
-DROP TABLE `users`;
+DROP TABLE `hellos`;
